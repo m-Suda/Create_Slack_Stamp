c48da3eb108b0532db710542bcdd7b2175404756
masak <oicwam.orz1228@gmail.com>
2019-02-09 01:35:04 +0900

    【Slack文字スタンプ自動生成スクリプト】DOMを別ファイルに切り出し

--- createSlackStamp.js
+++ createSlackStamp.js
@@ -3,6 +3,7 @@ const devices = require('puppeteer/DeviceDescriptors');
 const iPadMiniLandscape = devices['iPad Mini landscape'];
 const fetch = require('node-fetch');
 const fs = require('fs');
+const {DOM} = require('./dom/dom');
 
 const TARGET_IMAGE = /^https:\/\/emoji-gen\.ninja\/emoji\?align=center&back_color=FFFFFF00&color=000000FF&font=rounded-x-mplus-1p-black.*$/;
 const SAVE_IMAGE_PATH = __dirname + '/screenshot/emoji';
@@ -18,7 +19,7 @@ puppeteer.launch({
     args: [
         '--window-size=1024,768'
     ],
-    slowMo: 200
+    // slowMo: 200
 }).then(async browser => {
 
     const page = await browser.newPage();
@@ -26,23 +27,21 @@ puppeteer.launch({
     await page.goto('https://emoji-gen.ninja/#!/');
 
     // 文字画像生成
-    await page.$eval('body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.parameters > div.parameter.text > textarea',
-        input => {
-            // 値を入れた後にフォーカスを当てないとだめだった
-            input.value = 'てすと';
-            input.focus();
-        });
-    await page.click('body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.parameters > div.parameter.font > ul > li:nth-child(3)');
-    await page.$eval('body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.parameters > div.parameter.color > div.pickers > div:nth-child(1) > div > div.vue-color__chrome__chrome-body > div.vue-color__chrome__fields-wrap > div:nth-child(1) > div > div > input',
-        input => {
-            // 値を入れた後にフォーカスを当てないとだめだった
-            input.value = '#000000';
-            input.focus();
-        });
-    await page.click('body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.buttons > button');
+    await page.$eval(DOM.TEXT, input => {
+        // 値を入れた後にフォーカスを当てないとだめだった
+        input.value = 'てすと';
+        input.focus();
+    });
+    await page.click(DOM.FONT_BUTTON_ROUNDED);
+    await page.$eval(DOM.COLOR, input => {
+        // 値を入れた後にフォーカスを当てないとだめだった
+        input.value = '#000000';
+        input.focus();
+    });
+    await page.click(DOM.CREATE_BUTTON);
 
     // 画像保存
-    await page.waitForSelector('body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-result.expand-transition > div.preview > div > div.image > img');
+    await page.waitForSelector(DOM.CREATE_IMAGE);
     const res = await page._client.send('Page.getResourceTree');
     let images = [];
     res.frameTree.resources.forEach(element => {
---
+++ dom/dom.js
@@ -0,0 +1,7 @@
+module.exports.DOM = {
+    TEXT: 'body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.parameters > div.parameter.text > textarea',
+    FONT_BUTTON_ROUNDED: 'body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.parameters > div.parameter.font > ul > li:nth-child(3)',
+    COLOR: 'body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.parameters > div.parameter.color > div.pickers > div:nth-child(1) > div > div.vue-color__chrome__chrome-body > div.vue-color__chrome__fields-wrap > div:nth-child(1) > div > div > input',
+    CREATE_BUTTON: 'body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-generator > div.buttons > button',
+    CREATE_IMAGE: 'body > div > div.v-cloak.eg-emoji > div.v-cloak.eg-result.expand-transition > div.preview > div > div.image > img',
+};
